a50422a831ee6890bb0c5ea0e622c7861b129ae5
adilweb <adiltrain734@gmail.com>
2019-04-04 04:07:18 +0530

    Updates

---
+++ precache-manifest.8222dc2a84b4923fc5ea879cbe00feb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "842aa3e5b72be58d5944",
+    "url": "/my-youtube/static/js/runtime~main.8488af19.js"
+  },
+  {
+    "revision": "bc245b01e1da58623c1c",
+    "url": "/my-youtube/static/js/main.a2e2d52e.chunk.js"
+  },
+  {
+    "revision": "6b7a8549d14891e00d91",
+    "url": "/my-youtube/static/js/2.1e7b24b2.chunk.js"
+  },
+  {
+    "revision": "bc245b01e1da58623c1c",
+    "url": "/my-youtube/static/css/main.091abe4d.chunk.css"
+  },
+  {
+    "revision": "72bcaf3f376be24e11e18f36548471b3",
+    "url": "/my-youtube/index.html"
+  }
+];
